73c82f01beec0d4710993637ff70fe6ceb872400
pbedrosian <peter_bedrosian@Yahoo.com>
2020-04-16 08:06:15 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -15,5 +15,43 @@ class Dog
             breed TEXT
         );
         SQL
-        db[:conn]execute(sql)
+        DB[:conn].execute(sql)
+    end
+
+    def self.drop_table
+        DB[:conn].execute("DROP TABLE dogs")
+    end
+
+    def save
+        sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.breed) #calls the SQL to be exicuted and passes in the the name and breed
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0] #selects the row ID from the dog in the table
+      self #returns the instance of the Dog class
+    end
+
+    def self.create(hash) #takes in a hash of data
+       dog = Dog.new(name: hash[:name], breed: hash[:breed]) #uses the hash to create a new instance and assign attributes
+       dog.save #calls save method on new instance 
+    end 
+
+    def self.new_from_db(row)
+        new_dog = create(name:row[1],breed:row[2])    
+    end
+
+    def self.find_by_id(num)
+        row = DB[:conn].execute("SELECT * FROM dogs WHERE id = ?",num)[0][0] #sets row  = array of data from dogs table based on ID
+        new_dog = self.new(name: row[1], breed: row[2], id: row[0]) #creates a new Dog instance based on the row 
+    end
+
+    def self.find_or_create_by(row)
+        sql = <<-SQL
+        SELECT * FROM dogs
+        WHERE name = ? AND breed = ?
+        LIMIT 1
+        SQL
+        DB[:conn].execute(sql, self.name, self.breed)
+    end
 end
