e3689e910bda9cf4b08ec3a4b33e431086c9b538
Amber Williams <awilliams1234@live.com>
2020-04-17 16:01:45 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -38,9 +38,9 @@ class Dog
   end
 
   def self.new_from_db(row)
-    id = row[0]
-    name = row[1]
-    breed = row[2]
+    id: = row[0]
+    name: = row[1]
+    breed: = row[2]
     Dog.new
   end
 
