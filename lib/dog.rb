cdb7d20b2dceeed4264dc9df80e1dc8ae0e171cc
Amber Williams <awilliams1234@live.com>
2020-04-17 02:00:13 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+  attr_reader :id
+  attr_accessor :name, :breed
+
+  def initialize(id=nil, name, breed)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
