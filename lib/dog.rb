367335758adfcc41603165629282ee922e3d79be
Amber Williams <awilliams1234@live.com>
2020-04-17 16:21:24 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -60,8 +60,14 @@ class Dog
     end
     dog
   end
-  
-  def self.find_by_name
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT * FROM dogs WHERE name = ?
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
 
   end
 
